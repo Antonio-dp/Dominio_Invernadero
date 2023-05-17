c36c7630351788be6fa85f17b943091094d43c76
Antonio-dp <antonio.delpardo229161@potros.itson.edu.mx>
2023-05-17 00:30:58 -0700

    Update Alarma.java

--- Dominio_Invernadero/src/main/java/Entidades/Alarma.java
+++ Dominio_Invernadero/src/main/java/Entidades/Alarma.java
@@ -3,38 +3,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Entidades;
+
 import org.bson.types.ObjectId;
+
 /**
  *
  * @author tonyd
  */
 public class Alarma {
+
     private ObjectId _id;
     private String tipo;
     private float limiteInferior;
     private float limiteSuperior;
+    private String correo;
 
     public Alarma() {
     }
 
-    public Alarma(ObjectId idAlarma, String tipo, float limiteInferior, float limiteSuperior) {
+    public Alarma(ObjectId idAlarma, String tipo, float limiteInferior, float limiteSuperior, String correo) {
         this._id = idAlarma;
         this.tipo = tipo;
         this.limiteInferior = limiteInferior;
         this.limiteSuperior = limiteSuperior;
+        this.correo = correo;
     }
 
-    public Alarma(String tipo, float limiteInferior, float limiteSuperior) {
+    public Alarma(String tipo, float limiteInferior, float limiteSuperior, String correo) {
         this.tipo = tipo;
         this.limiteInferior = limiteInferior;
         this.limiteSuperior = limiteSuperior;
+        this.correo = correo;
     }
-    
 
     public Alarma(ObjectId idAlarma) {
         this._id = idAlarma;
     }
-    
+
     public ObjectId getId() {
         return _id;
     }
@@ -66,5 +71,13 @@ public class Alarma {
     public void setLimiteSuperior(float limiteSuperior) {
         this.limiteSuperior = limiteSuperior;
     }
-    
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
 }
